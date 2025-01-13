7eb2ff248cf2b077c1048b434ba059bdade1cda2
Castor <castorsh3m@gmail.com>
2025-01-12 23:28:06 -0700

    Lab 1 optimization/testing

---
+++ Lab1/tests/test_speedup.sh
@@ -0,0 +1,41 @@
+cd "/home/user_20/Lab1/Development Kit Lab1"
+make all
+
+echo "n=100"
+./matrixgen -s 100
+echo "> SEQUENTIAL"
+./demo_seq
+echo "> PARALLEL p=4"
+./demo 4
+echo "> PARALLEL p=25"
+./demo 25
+echo "> PARALLEL p=100"
+./demo 100
+
+echo "n=1000"
+./matrixgen -s 1000
+echo "> SEQUENTIAL"
+./demo_seq
+echo "> PARALLEL p=4"
+./demo 4
+echo "> PARALLEL p=25"
+./demo 25
+echo "> PARALLEL p=100"
+./demo 100
+
+echo "n=2048"
+./matrixgen -s 2048
+echo "> SEQUENTIAL"
+./demo_seq
+echo "> PARALLEL p=4"
+./demo 4
+echo "> PARALLEL p=64"
+./demo 64
+echo "> PARALLEL p=1024"
+./demo 1024
+
+./matrixgen -s 20000
+echo "> SEQUENTIAL"
+./demo_seq
+echo "> PARALLEL p=10000"
+./demo 10000
---
+++ Lab1/tests/validate_outputs.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+
+
+INPUT = "/home/user_20/Lab1/Development Kit Lab1/data_input"
+OUTPUT = "/home/user_20/Lab1/Development Kit Lab1/data_output"
+
+# get input mats
+with open(INPUT, 'r') as f:
+    lines = f.readlines()
+    n = int(lines[0])
+    lines_A = lines[2:2+n]
+    lines_B = lines[3+n:3+2*n]
+
+mat_A = np.array([x.strip().split('\t') for x in lines_A], dtype=int)
+mat_B = np.array([x.strip().split('\t') for x in lines_B], dtype=int)
+
+# get output mat
+with open(OUTPUT, 'r') as f:
+    lines_C = f.readlines()[2:-1]
+
+mat_C = np.array([x.strip().split('\t') for x in lines_C], dtype=int)
+
+# multiply
+my_C = np.matmul(mat_A, mat_B)
+
+# print(my_C)
+# print(mat_C)
+if np.all(my_C == mat_C):
+    print("> answer ok")
+else:
+    print("> ANSWER INCORRECT")
